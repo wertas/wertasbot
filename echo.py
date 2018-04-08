450bda827e2c918fdf889136259501bceaa55e74
wertas <wertas>
2018-04-09 01:39:58 +0300

    eee

--- echo.py
+++ echo.py
@@ -5,57 +5,57 @@ import time
 
 bot = telebot.TeleBot(config.token)
 
-
-
-# @bot.message_handler(content_types=["text"])
-# def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-	# print(message.chat.id ,' >> ' , message.text)
-	# if message.text.find('+') != -1:
-		# a,b = message.text.split('+')
-		# try:
-			# a = float(a)
-			# b = float(b)
-			# bot.send_message(message.chat.id, float(a)+float(b))
-			# print(message.chat.id ,' << ' , float(a)+float(b))
-		# except ValueError:
-			# bot.send_message(message.chat.id, 'Not float numbers')
-	# elif message.text.find('-') != -1:
-		# a,b = message.text.split('-')
-		# try:
-			# a = float(a)
-			# b = float(b)
-			# bot.send_message(message.chat.id, float(a)-float(b))
-			# print(message.chat.id ,' << ' , float(a)-float(b))
-		# except ValueError:
-			# bot.send_message(message.chat.id, 'Not float numbers')
-	# elif message.text.find('*') != -1:
-		# a,b = message.text.split('*')
-		# try:
-			# a = float(a)
-			# b = float(b)
-			# bot.send_message(message.chat.id, float(a)*float(b))
-			# print(message.chat.id ,' << ' , float(a)*float(b))
-		# except ValueError:
-			# bot.send_message(message.chat.id, 'Not float numbers')
-	# elif message.text.find('/') != -1:
-		# a,b = message.text.split('/')
-		# try:
-			# a = float(a)
-			# b = float(b)
-			# bot.send_message(message.chat.id, float(a)/float(b))
-			# print(message.chat.id ,' << ' , float(a)/float(b))
-		# except ValueError:
-			# bot.send_message(message.chat.id, 'Not float numbers')
-	# bot.send_message(message.chat.id, input())
-	
-
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-	bot.send_message(message.chat.id, 'daI can calculate every two numbers, try it (example: 2+2)')
-	
+	bot.send_message(message.chat.id, 'I can calculate every two numbers, try it (example: 2+2)')
+
 @bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    bot.send_message(message.chat.id, message.text)
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+	print(message.chat.id ,' >> ' , message.text)
+	if message.text.find('+') != -1:
+		a,b = message.text.split('+')
+		try:
+			a = float(a)
+			b = float(b)
+			bot.send_message(message.chat.id, float(a)+float(b))
+			print(message.chat.id ,' << ' , float(a)+float(b))
+		except ValueError:
+			bot.send_message(message.chat.id, 'Not float numbers')
+	elif message.text.find('-') != -1:
+		a,b = message.text.split('-')
+		try:
+			a = float(a)
+			b = float(b)
+			bot.send_message(message.chat.id, float(a)-float(b))
+			print(message.chat.id ,' << ' , float(a)-float(b))
+		except ValueError:
+			bot.send_message(message.chat.id, 'Not float numbers')
+	elif message.text.find('*') != -1:
+		a,b = message.text.split('*')
+		try:
+			a = float(a)
+			b = float(b)
+			bot.send_message(message.chat.id, float(a)*float(b))
+			print(message.chat.id ,' << ' , float(a)*float(b))
+		except ValueError:
+			bot.send_message(message.chat.id, 'Not float numbers')
+	elif message.text.find('/') != -1:
+		a,b = message.text.split('/')
+		try:
+			a = float(a)
+			b = float(b)
+			bot.send_message(message.chat.id, float(a)/float(b))
+			print(message.chat.id ,' << ' , float(a)/float(b))
+		except ValueError:
+			bot.send_message(message.chat.id, 'Not float numbers')
+	bot.send_message(message.chat.id, input())
+	
+
+
+	
+# @bot.message_handler(func=lambda message: True)
+# def echo_message(message):
+    # bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
 	while True:
